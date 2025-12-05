ae7ec5dda01d5b377db28ecf57f21fbbdc2c0f79
katiagarcia438198-art <katiagarcia438198@gmail.com>
2025-12-05 11:32:37 -0600

    Refactor SWAS10102025.py for improved readability
    Removed unnecessary comments and added spacing for readability.

--- SWAS10102025.py
+++ SWAS10102025.py
@@ -1,13 +1,26 @@
 #importar la biblioteca
+
 import tkinter as tk 
 from tkinter import ttk
+
+
 
 #crear la ventana en python 
 ventana=tk.Tk()
 #configuramos la ventana 
 ventana.title("Esta es mi primera ventana")
 ventana.geometry("700x800")
+
+
+
+
 #agregamos  los widgets
+#colocando la informacion en columnas y filas para
+organizarla
+
+
+
+
 ttk.Label(ventana,text="APLICA METODOLOGIAS AGILES PARA EL DESARROLLO DE SOFTWARE").grid(column=0,row=1)
 ttk.Label(ventana,text="Equipo numero 3").grid(column=0,row=2)
 ttk.Label(ventana,text="Karla sanchez Morales").grid(column=0,row=3)
@@ -15,6 +28,12 @@ ttk.Label(ventana,text="Katia Garcia Yañez").grid(column=0,row=4)
 ttk.Label(ventana,text="Yosany Gonzalez Perez").grid(column=0,row=5)
 ttk.Label(ventana,text="Evelyn Juarez Perez").grid(column=0,row=6)
 
+
+
+
+
+
 #Activamos la ventana
 ventana.resizable(0,0)
 ventana.mainloop()
+
